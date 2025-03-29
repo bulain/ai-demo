34e289e5534755ec0d8b9caf1be58b1ad5e7c9c9
Bulain Peng <bulain@gmail.com>
2025-03-29 22:25:12 +0800

    update autogen demo

--- tests/agent/autogen/autogen-demo.py
+++ tests/agent/autogen/autogen-demo.py
@@ -1,16 +1,20 @@
 import asyncio
+
 from autogen_agentchat.agents import AssistantAgent
-from autogen_core.models import ModelInfo
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 from decouple import config
 
 
 async def main() -> None:
-    model_info = ModelInfo(vision=True, function_calling=True, json_output=True, family="o1")
     model_client = OpenAIChatCompletionClient(model="deepseek-chat",
                                               api_key=config("DS_API_KEY"),
                                               base_url=config("DS_BASE_URL"),
-                                              model_info=model_info)
+                                              model_info={
+                                                  "vision": True,
+                                                  "function_calling": True,
+                                                  "json_output": True,
+                                                  "family": "o1",
+                                              })
     agent = AssistantAgent("assistant", model_client=model_client)
     print(await agent.run(task="Say 'Hello World!'"))
     await model_client.close()
--- tests/agent/autogen/autogen-team.py
+++ tests/agent/autogen/autogen-team.py
@@ -1,20 +1,24 @@
 import asyncio
+
 from autogen_agentchat.agents import UserProxyAgent
 from autogen_agentchat.conditions import TextMentionTermination
 from autogen_agentchat.teams import RoundRobinGroupChat
 from autogen_agentchat.ui import Console
-from autogen_ext.models.openai import OpenAIChatCompletionClient
 from autogen_ext.agents.web_surfer import MultimodalWebSurfer
-from autogen_core.models import ModelInfo
+from autogen_ext.models.openai import OpenAIChatCompletionClient
 from decouple import config
 
 
 async def main() -> None:
-    model_info = ModelInfo(vision=False, function_calling=True, json_output=True, family="r1")
     model_client = OpenAIChatCompletionClient(model="deepseek-chat",
                                               api_key=config("DS_API_KEY"),
                                               base_url=config("DS_BASE_URL"),
-                                              model_info=model_info)
+                                              model_info={
+                                                  "vision": True,
+                                                  "function_calling": True,
+                                                  "json_output": True,
+                                                  "family": "o1",
+                                              })
     # The web surfer will open a Chromium browser window to perform web browsing tasks.
     web_surfer = MultimodalWebSurfer("web_surfer", model_client, headless=True, animate_actions=True)
     # The user proxy agent is used to get user input after each step of the web surfer.
